bc4d54525e446b942b917386d1d737de5a0642bd
mario <mario.skraban@gmail.com>
2023-02-05 00:08:56 +0100

    Initial commit

---
+++ project/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'google',
+        "plugin:vue/vue3-recommended",
+        "prettier"
+    ],
+    plugins : [
+      'vue',
+    ],
+    rules: {
+        'indent': ['warn', 4],
+        'require-jsdoc': ['off'],
+        'no-console': ['off'],
+        'no-invalid-this': ['off'],
+        'max-len': ['warn', 120, 2],
+        'vue/script-indent': ['warn', 4, {'baseIndent': 0}],
+        'vue/html-indent': ['warn', 4, {'baseIndent': 1}],
+        'vue/no-v-html': ['off'],
+    }
+}
