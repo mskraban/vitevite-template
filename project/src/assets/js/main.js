8cfa404ea168081699f0d3793bfcde61e3d65192
mario <mario.skraban@gmail.com>
2023-02-11 14:16:56 +0100

    Custom component

--- project/src/assets/js/main.js
+++ project/src/assets/js/main.js
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import '../scss/main.scss'
 import App from './components/App.vue'
+import { defineCustomElement } from 'vue'
 
 import Home from './views/HomePage.vue'
 import About from './views/AboutPage.vue'
 import Brazil from './views/BrazilPage.vue'
+
+// Vue generates a new HTML element class from the component definition.
+const AboutExample = defineCustomElement(About)
+
+// Register the custom element so that it can be used as <dark-mode-switch>.
+customElements.define('about-example', AboutExample)
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
